613fc2c697d8627f7381550a1a2770b2ebfd71b4
Subhankar Sarkar <93681750+sarkarsubho@users.noreply.github.com>
2024-03-13 06:22:44 +0530

    ..

--- client/src/constants/sampleData.js
+++ client/src/constants/sampleData.js
@@ -66,3 +66,51 @@ export const sampleUsers = [
     _id: "2",
   },
 ];
+
+export const dashboardData = [
+  {
+    _id: "1",
+    avatar:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "john deo",
+    username: "alex001",
+    friends: 50,
+    groups: 5,
+  },
+  {
+    _id: "2",
+    avatar:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "john deo",
+    username: "alex001",
+    friends: 50,
+    groups: 5,
+  },
+  {
+    _id: "3",
+    avatar:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "john deo",
+    username: "alex001",
+    friends: 50,
+    groups: 5,
+  },
+  {
+    _id: "4",
+    avatar:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "john deo",
+    username: "alex001",
+    friends: 50,
+    groups: 5,
+  },
+  {
+    _id: "5",
+    avatar:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "john deo",
+    username: "alex001",
+    friends: 50,
+    groups: 5,
+  },
+];
--- client/src/pages/admin/UserManagement.jsx
+++ client/src/pages/admin/UserManagement.jsx
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AdminLayout from "../../components/Layout/AdminLayout";
+import Table from "../../components/shared/Table";
+import { Avatar } from "@mui/material";
+
+const columns = [
+  {
+    field: "id",
+    headerName: "ID",
+    headerClassName: "table-header",
+    width: 200,
+  },
+
+  {
+    field: "avatar",
+    headerName: "Avatar",
+    headerClassName: "table-header",
+    width: 150,
+    renderCell: (params) => (
+      <Avatar alt={params.row.name} src={params.row.avatar}></Avatar>
+    ),
+  },
+  {
+    field: "name",
+    headerName: "Name",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "username",
+    headerName: "Username",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "friends",
+    headerName: "Friends",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "groups",
+    headerName: "Groups",
+    headerClassName: "table-header",
+    width: 200,
+  },
+];
 
 const UserManagement = () => {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {}, []);
   return (
     <AdminLayout>
-      <div>UserManagement</div>
+      <Table heading={"All Users"} columns={columns} rows={rows}></Table>
     </AdminLayout>
   );
 };
