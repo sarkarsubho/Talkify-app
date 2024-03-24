6b60d7fc27c0f661daafdce990c71f698f31c66d
Subhankar Sarkar <93681750+sarkarsubho@users.noreply.github.com>
2024-03-24 23:42:34 +0530

    frontend and backend marging started

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,15 +3,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { App } from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <CssBaseline />
-      {/* using preventDefault on app context menu to prevent the right click behavior in the app  */}
-      <div onContextMenu={(e) => e.preventDefault()}>
-        <App />
-      </div>
-    </HelmetProvider>
+    <Provider store={store}>
+      <HelmetProvider>
+        <CssBaseline />
+        {/* using preventDefault on app context menu to prevent the right click behavior in the app  */}
+        <div onContextMenu={(e) => e.preventDefault()}>
+          <App />
+        </div>
+      </HelmetProvider>
+    </Provider>
   </React.StrictMode>
 );
