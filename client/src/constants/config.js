470b20db2ebfe7ac5213df12561d8bfc641b46da
Subhankar Sarkar <93681750+sarkarsubho@users.noreply.github.com>
2024-05-11 06:28:09 +0530

    edit config todo

--- client/src/constants/config.js
+++ client/src/constants/config.js
@@ -1,9 +1,5 @@
 export const server =import.meta.env.VITE_SERVER
 
-// fix alert listner, add refatch listner
-//  something went wrong at the time of accept friend request
-// add loading and disabled the register button
-
 // **
 // can add admin button at for change the admin in groups
 // delete message functionality.
